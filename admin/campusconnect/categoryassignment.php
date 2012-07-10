bc0d948bb6bd56d5f9be5e8eb850bb5073fbc379
Andrew Davidson <andrew.davidson@synergy-learning.com>
2012-07-10 16:43:00 +0100

    new category assignment form - needs finished

--- admin/campusconnect/categoryassignment.php
+++ admin/campusconnect/categoryassignment.php
@@ -24,6 +24,7 @@
 
 require_once('../../config.php');
 require_once($CFG->libdir.'/adminlib.php');
+require_once("$CFG->libdir/formslib.php");
 require_once($CFG->dirroot.'/local/campusconnect/connect.php');
 require_once($CFG->dirroot.'/admin/campusconnect/lib.php');
 
@@ -37,134 +38,88 @@ admin_externalpage_setup('campusconnectcategoryassignment');
 require_login();
 require_capability('moodle/site:config', get_context_instance(CONTEXT_SYSTEM));
 
-echo $OUTPUT->header();
-echo $OUTPUT->heading(get_string('pluginname', 'local_campusconnect'));
+class campusconnect_category_form extends moodleform {
 
-$categories = get_categories();
-foreach ($categories as $category) {
-    $cats[$category->id] = $category->name;
+    public function definition() {
+        global $CFG, $cats;
+
+        $mform =& $this->_form;
+
+        $mform->addElement('header', 'categoryassignment', get_string('categoryassignment', 'local_campusconnect'));
+
+        $categories = get_categories();
+        foreach ($categories as $category) {
+            $cats[$category->id] = $category->name;
+        }
+        $mform->addElement('select', 'importcat', get_string('importcat', 'local_campusconnect'), $cats);
+
+        $mform->addElement('select', 'attributename', get_string('attributename', 'local_campusconnect'), array('1' => 'Community', '2' => 'Participant ID'));
+
+        $radioarray=array();
+        $radioarray[] = &MoodleQuickForm::createElement('radio', 'cc_mapping', '', get_string('fixedvalue', 'local_campusconnect'), 'mapping_fixed', 'onclick=cc_switch_mapping_fixed()');
+        $radioarray[] = &MoodleQuickForm::createElement('radio', 'cc_mapping', '', get_string('daterange', 'local_campusconnect'), 'mapping_date', 'onclick=cc_switch_mapping_date()');
+        $mform->addGroup($radioarray, 'radioar', get_string('mappingtype', 'local_campusconnect'), array(' '), false);
+        $mform->setDefault('cc_mapping', 'mapping_fixed');
+
+        $mform->addElement('text', 'attribute', get_string('attribute', 'local_campusconnect'));
+
+        $mform->addElement('date_selector', 'daterangefrom', get_string('from'));
+        $mform->addElement('date_selector', 'daterangeto', get_string('to'));
+
+        $mform->disabledIf('attribute', 'cc_mapping', 'eq', 'mapping_date');
+        $mform->disabledIf('daterangefrom', 'cc_mapping', 'eq', 'mapping_fixed');;
+        $mform->disabledIf('daterangeto', 'cc_mapping', 'eq', 'mapping_fixed');
+
+        $this->add_action_buttons();
+
+    }
 }
 
-print get_string('currentassignments', 'local_campusconnect');
+$mform = new campusconnect_category_form();
 
-print '<table class="generaltable"><thead><tr><th class="header">Current Assignments</th></tr></thead></table>';
+if ($mform->is_cancelled()) {
 
-print '<h2>'.get_string('newassignment', 'local_campusconnect').'</h2>';
+    redirect("{$CFG->wwwroot}/admin/campusconnect/categoryassignment.php", '', 0);
 
-?>
-<form action="" method="POST" id="currentassignments" class="mform">
-    <div class="settingsform">
-        <fieldset class="clearfix" name="connectionsettings">
-            <legend><?php print get_string('categoryassignment', 'local_campusconnect') ?></legend>
-            <div class="fcontainer clearfix">
-                <div class="fitem clearfix">
-                    <div class="fitemtitle">
-                        <label for="importcat">
-                            <?php print get_string('importcat', 'local_campusconnect') ?>
-                            <?php print "<img src='".$OUTPUT->pix_url('req')."' alt='req' />" ?>
-                        </label>
-                    </div>
-                    <div class="felement ftext">
-                        <select name="importcat">
-                            <?php print_option($cats, $_POST); ?>
-                        </select>
-                    </div>
-                </div>
-            </div>
-            <div class="fcontainer clearfix">
-                <div class="fitem clearfix">
-                    <div class="fitemtitle">
-                        <label for="attrname">
-                            <?php print get_string('attributename', 'local_campusconnect') ?>
-                            <?php print "<img src='".$OUTPUT->pix_url('req')."' alt='req' />" ?>
-                        </label>
-                    </div>
-                    <div class="felement ftext">
-                        <select name="attrname">
-                            <?php print_option(array('1' => 'Community', '2' => 'Participant ID'), $_POST); ?>
-                        </select>
-                    </div>
-                </div>
-            </div>
-            <div class="fitem clearfix">
-                            <div class="fitemtitle">
-                                <label for="maptype">
-                                    <?php print get_string('mappingtype', 'local_campusconnect') ?>
-                                    <?php print "<img src='".$OUTPUT->pix_url('req')."' alt='req' />" ?>
-                                </label>
-                            </div>
-                            <div class="felement ftext">
-                                <div class="fitem clearfix">
-                                    &nbsp;
-                                    <input type="radio" onclick="cc_switchAuthCert();"
-                                        <?php if ($post['cc_auth'] == 'cc_auth_cert' || !isset($post['cc_auth'])) {
-                                            print 'checked';
-                                        } ?>
-                                        name="cc_auth" value="cc_auth_cert" />
-                                    <?php print get_string('fixedvalue', 'local_campusconnect') ?>
-                                    <div id="cc_auth_cert"
-                                    <?php if ($post['cc_auth'] == 'cc_auth_user') {
-                                        print 'style="display: none"';
-                                    } ?> class="clearfix">
-                                        <div class="fitem clearfix">
-                                            <div class="fitemtitle">
-                                                <label for="fixedvalue">
-                                                    <?php print get_string('attribute', 'local_campusconnect') ?>
-                                                </label>
-                                            </div>
-                                            <div class="felement ftext">
-                                                <input type="text" name="fixedvalue"/>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="fitem clearfix">
-                                    &nbsp;
-                                    <input type="radio" onclick="cc_switchAuthUser();"
-                                        <?php if ($post['cc_auth'] == 'cc_auth_user') {
-                                            print 'checked';
-                                        } ?>
-                                        name="cc_auth" value="cc_auth_user" />
-                                    <?php print get_string('daterange', 'local_campusconnect') ?>
-                                    <div id="cc_auth_user"
-                                        <?php if ($post['cc_auth'] == 'cc_auth_cert' || !isset($post['cc_auth'])) {
-                                            print 'style="display:none"';
-                                        } ?> class="clearfix">
-                                        <div class="fitem felement fselect clearfix">
-                                            <label>Day</label>
-                                            <select name="startdate[day]" id="id_startdate_day">
-                                                <?php print_option(range(1, 31), $_POST); ?>
-                                            </select>&nbsp;
-                                            <label>Month</label>
-                                            <select name="startdate[month]" id="id_startdate_month">
-                                                <?php for ($i = 1; $i <= 12; $i++) {
-                                                        $months[$i] = date('F', strtotime("{$i}/01/2000"));
-                                                    }
-                                                    print_option($months , $_POST);
-                                                ?>
-                                            </select>&nbsp;
-                                            <label>Year</label>
-                                            <select name="startdate[year]" id="id_startdate_year">
-                                                <?php print_option(range(2000, 2025), $_POST, 2012); ?>
-                                            </select>
-                                        </div>
-                                    </div>
-                                </div>
-                                <script type="text/javascript">
-                                    function cc_switchAuthCert() {
-                                        document.getElementById('cc_auth_cert').style.display='block';
-                                        document.getElementById('cc_auth_user').style.display='none';
-                                    }
+} else if ($post=$mform->get_data()) {
 
-                                    function cc_switchAuthUser() {
-                                        document.getElementById('cc_auth_cert').style.display='none';
-                                        document.getElementById('cc_auth_user').style.display='block';
-                                    }
-                                </script>
-                            </div>
-        </fieldset>
-    </div>
-</form>
-<?php
+    print 'TODO';
+    //print_object($post);
+    //exit;
+    //redirect("{$CFG->wwwroot}/admin/campusconnect/categoryassignment.php", '', 0);
 
-echo $OUTPUT->footer();
+} else {
+
+    echo $OUTPUT->header();
+    echo $OUTPUT->heading(get_string('pluginname', 'local_campusconnect'));
+
+    print get_string('currentassignments', 'local_campusconnect');
+
+    print '<table class="generaltable"><thead><tr><th class="header">Current Assignments</th></tr></thead></table>';
+
+    print '<h2>'.get_string('newassignment', 'local_campusconnect').'</h2>';
+
+    $mform->display();
+
+    ?>
+    <script type="text/javascript">
+
+        cc_switch_mapping_fixed();
+
+        function cc_switch_mapping_fixed() {
+            document.getElementById('fitem_id_attribute').style.display='block';
+            document.getElementById('fitem_id_daterangefrom').style.display='none';
+            document.getElementById('fitem_id_daterangeto').style.display='none';
+        }
+
+        function cc_switch_mapping_date() {
+            document.getElementById('fitem_id_attribute').style.display='none';
+            document.getElementById('fitem_id_daterangefrom').style.display='block';
+            document.getElementById('fitem_id_daterangeto').style.display='block';
+        }
+    </script>
+    <?php
+
+    echo $OUTPUT->footer();
+
+}

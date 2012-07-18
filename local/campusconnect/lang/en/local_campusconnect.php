f31de8f2e38d1d9bd2db038136cb8d4a2a7974cf
Davo Smith <git@davosmith.co.uk>
2012-07-18 16:04:26 +0100

    Language strings now in alphabetical order again

--- local/campusconnect/lang/en/local_campusconnect.php
+++ local/campusconnect/lang/en/local_campusconnect.php
@@ -22,12 +22,36 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+$string['activationperiod'] = 'Activation Period';
+$string['activationperioddesc'] = 'New ECS user accounts are limited to the session duration. After assigning a user to a course the activation period will be extended by the given number of months';
+$string['addecs'] = 'Add ECS';
+$string['allecs'] = 'All ECS';
+$string['assignmenttocategories'] = 'Assignment To Categories';
+$string['attribute'] = 'Attribute';
+$string['attributename'] = 'Attribute Name';
+$string['authenticationtype'] = 'Authentication Type';
+$string['cacertificate'] = 'CA Certificate';
+$string['campusmanagement'] = 'Campus Management';
 $string['cannotbeempty'] = 'The field {$a} cannot be empty';
+$string['categoryassignment'] = 'Category Assignment';
+$string['categoryid'] = 'Category ID';
+$string['categoryiddesc'] = 'Please enter the ID of the category where new Course Links will be created';
+$string['certificatebase'] = 'Certificate/Base';
+$string['certificatekey'] = 'Certificate Key';
+$string['clientcertificate'] = 'Client Certificate';
+$string['connectionsettings'] = 'Connection Settings';
+$string['contentnotificationdesc'] = 'Enter one or more usernames of users (comma seperated) that will be informed about new ECS content by e-mail.';
+$string['course'] = 'Course';
+$string['coursenotificationdesc'] = 'Enter one or more usernames of users (comma seperated) that will be informed about new approved courses by e-mail.';
+$string['currentassignments'] = 'Current Assignments';
+$string['daterange'] = 'Date Range';
+$string['ecs'] = 'ECS';
+$string['ecscourselink'] = 'ECS Course Link';
+$string['ecsdatamapping'] = 'ECS Data Mapping';
 $string['ecserror_body'] = 'An error occurred whilst attempting to connect to the ECS server \'{$a->ecsname}\' ({$a->ecsid}). The error was:
 {$a->msg}';
 $string['ecserror_subject'] = 'Error connecting to the ECS server';
 $string['error'] = 'Error: {$a}';
-
 $string['field_courseid'] = 'Course ID';
 $string['field_coursetype'] = 'Course type';
 $string['field_credits'] = 'Credits';
@@ -36,76 +60,38 @@ $string['field_organisation'] = 'Organisation';
 $string['field_semesterhours'] = 'Semester hours';
 $string['field_status'] = 'Status';
 $string['field_term'] = 'Term';
-
-$string['localfieldnotfound'] = 'The local field {$a} does not exist';
-$string['messageprovider:ecserror'] = 'ECS Connection problems';
-$string['pluginname'] = 'CampusConnect';
-$string['remotefieldnotfound'] = 'The remote field {$a} does not exist';
-
-
-
-//Strings for admin settings
-
-$string['settings'] = 'Settings';
-$string['ecs'] = 'ECS';
-$string['addecs'] = 'Add ECS';
-$string['allecs'] = 'All ECS';
-$string['participants'] = 'Participants';
-$string['ecsdatamapping'] = 'ECS Data Mapping';
-$string['assignmenttocategories'] = 'Assignment To Categories';
-$string['importedcourses'] = 'Imported Courses';
-$string['releasedcourses'] = 'Released Courses';
-
-$string['connectionsettings'] = 'Connection Settings';
-$string['name'] = 'Name';
-$string['url'] = 'URL';
-$string['protocol'] = 'Protocol';
-$string['port'] = 'Port';
-$string['authenticationtype'] = 'Authentication Type';
-$string['certificatebase'] = 'Certificate/Base';
-$string['clientcertificate'] = 'Client Certificate';
-$string['certificatekey'] = 'Certificate Key';
-$string['keypassword'] = 'Key Password';
-$string['cacertificate'] = 'CA Certificate';
-$string['usernamepassword'] = 'Username/Password';
-$string['username'] = 'Username';
-$string['password'] = 'Password';
-
-$string['ecscourselink'] = 'ECS Course Link';
-$string['course'] = 'Course';
-$string['campusmanagement'] = 'Campus Management';
-
-$string['currentassignments'] = 'Current Assignments';
-$string['newassignment'] = 'New Assignment';
-$string['importcat'] = 'Import Category';
-$string['categoryassignment'] = 'Category Assignment';
-$string['attributename'] = 'Attribute Name';
-$string['mappingtype'] = 'Mapping Type';
 $string['fixedvalue'] = 'Fixed Value';
-$string['attribute'] = 'Attribute';
-$string['daterange'] = 'Date Range';
-
+$string['importcat'] = 'Import Category';
+$string['importedcourses'] = 'Imported Courses';
+$string['keypassword'] = 'Key Password';
+$string['localfieldnotfound'] = 'The local field {$a} does not exist';
 $string['localsettings'] = 'Local Settings';
-$string['pollingtime'] = 'Polling Time';
+$string['mappingtype'] = 'Mapping Type';
+$string['messageprovider:ecserror'] = 'ECS Connection problems';
 $string['minutes'] = 'Minutes';
-$string['seconds'] = 'seconds';
+$string['months'] = 'months';
+$string['name'] = 'Name';
+$string['newassignment'] = 'New Assignment';
+$string['notifcationaboutecsusers'] = 'Notifications about ECS users';
+$string['notificationaboutapprovedcourses'] = 'Notifications about approved courses';
+$string['notificationaboutnewecontent'] = 'Notifications about new E-Content';
+$string['notifications'] = 'Notifications';
+$string['participants'] = 'Participants';
+$string['password'] = 'Password';
+$string['pleasefilloutallrequiredfields'] = 'Please fill out all required fields';
+$string['pluginname'] = 'CampusConnect';
+$string['pollingtime'] = 'Polling Time';
 $string['pollingtimedesc'] = 'Please define the polling time for creation and update of Course Links';
-$string['categoryid'] = 'Category ID';
-$string['categoryiddesc'] = 'Please enter the ID of the category where new Course Links will be created';
-
-$string['useraccountsettings'] = 'User Account Settings';
+$string['port'] = 'Port';
+$string['protocol'] = 'Protocol';
+$string['releasedcourses'] = 'Released Courses';
+$string['remotefieldnotfound'] = 'The remote field {$a} does not exist';
 $string['roleassignments'] = 'Role Assignments';
 $string['roleassignmentsdesc'] = 'The chosen role will be assigned to newly created ECS user accounts';
-$string['activationperiod'] = 'Activation Period';
-$string['months'] = 'months';
-$string['activationperioddesc'] = 'New ECS user accounts are limited to the session duration. After assigning a user to a course the activation period will be extended by the given number of months';
-
-$string['notifications'] = 'Notifications';
-$string['notifcationaboutecsusers'] = 'Notifications about ECS users';
+$string['seconds'] = 'seconds';
+$string['settings'] = 'Settings';
+$string['url'] = 'URL';
+$string['useraccountsettings'] = 'User Account Settings';
+$string['username'] = 'Username';
+$string['usernamepassword'] = 'Username/Password';
 $string['usernotificationdesc'] = 'Enter one or more usernames of users (comma seperated) that will be informed about new ECS users by e-mail.';
-$string['notificationaboutnewecontent'] = 'Notifications about new E-Content';
-$string['contentnotificationdesc'] = 'Enter one or more usernames of users (comma seperated) that will be informed about new ECS content by e-mail.';
-$string['notificationaboutapprovedcourses'] = 'Notifications about approved courses';
-$string['coursenotificationdesc'] = 'Enter one or more usernames of users (comma seperated) that will be informed about new approved courses by e-mail.';
-
-$string['pleasefilloutallrequiredfields'] = 'Please fill out all required fields';

1301c4b4ae7dd20769858e23426065d9a5bba2eb
yair <yair.spielmann@synergy-learning.com>
2012-10-16 16:00:23 +0100

    CC-15 Role mapping: Get role function

---
+++ local/campusconnect/rolemap.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of the CampusConnect plugin for Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Handles role mapping
+ *
+ * @package   local_campusconnect
+ * @copyright 2012 Yair Spielmann, Synergy Learning
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/*
+ * For a given CampusConnect role, return the id of its role mapping (or false when not found)
+ * @param string $role
+ */
+function get_roleid($role) {
+    global $DB;
+    return $DB->get_field('local_campusconnect_rolemap', 'moodleroleid', array('ccrolename' => $role));
+}
